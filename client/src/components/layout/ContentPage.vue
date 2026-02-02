4980df505068b4d2b3b7a7a97dff7176f3b4eebc
Louis Eveillard <louis-ev@users.noreply.github.com>
2026-02-02 14:56:19 +0100

    FIX / content page

---
+++ client/src/components/layout/ContentPage.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="_contentPage">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  name: "ContentPage",
+  props: {},
+  components: {},
+  data() {
+    return {};
+  },
+  created() {},
+  mounted() {},
+  beforeDestroy() {},
+  watch: {},
+  computed: {},
+  methods: {},
+};
+</script>
+<style lang="scss" scoped>
+._contentPage {
+  padding: var(--spacing) 0;
+  width: 90%;
+  max-width: 560px;
+  padding: calc(var(--spacing) * 2);
+  margin: 0 auto;
+
+  ::v-deep {
+    p {
+      margin: calc(var(--spacing) * 1) 0;
+    }
+  }
+}
+</style>
