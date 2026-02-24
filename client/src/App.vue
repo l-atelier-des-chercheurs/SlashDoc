d362bf42173a1b7f4b46d777ecf215dc7ac81ffc
Louis Eveillard <louis-ev@users.noreply.github.com>
2026-02-24 11:07:23 +0100

    Update App.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -163,7 +163,7 @@ export default {
   --c-bodybg: hsl(40, 20%, 94%);
   --c-bodybg: hsl(40, 0%, 94%);
 
-  --plyr-color-main: var(--c-bleumarine);
+  --plyr-color-main: var(--gr-200);
 
   // --sharedfolder-bg: ;
 
